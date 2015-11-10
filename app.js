1b3748048b251e4fccab73824b6191121a780972
Zack Elias <zelias500@gmail.com>
2015-11-10 13:56:04 -0500

    joke edit

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ var jokes = [
 "Q: What's the Object-Oriented way to become wealthy? A: Inheritance",
 
 "The bartender says 'Success, but you're not ready!' A Javascript function walks into a bar",
-"If you're happy and you know it, syntax error",
+"isNaN('leavened, oven-baked flatbread') // true",
 ' typeof ["hip", "hip"]  ',
 "To understand what recursion is, you must first understand recursion",
 "Q: How do you comfort a Javascript bug? A: You console it"
